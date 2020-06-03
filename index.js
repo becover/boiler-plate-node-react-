4dc1f159214f3126bbdfb3d5fbf8b6da90c08cdc
becover <sedi.september@gmail.com>
2020-06-04 00:35:31 +0900

    add init setting mongoDB, mongoose, body-parser and ignore db password(dev.js)

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
